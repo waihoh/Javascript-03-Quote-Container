7ddeec71ca518b85b3ad3aff0517ee5e59837d9c
Tang Wai Hoh <waihoh@gmail.com>
2022-02-18 22:35:59 +0800

    event listener

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+
 // let apiQuotes = [];
 
 // Show new quote
@@ -5,8 +11,31 @@ function newQuote() {
     // pick a random quote from apiQuotes array
     // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
-    console.log(quote);
+    // Check if Author field is blank and replace it with 'unknown'
+    if (!quote.author) {
+        authorText.textContent = "unknown";
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    // check Quote length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    quoteText.textContent = quote.text;
 }
+
+// Tweet quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 // // Get Quotes From API
 // async function getQuotes() {
