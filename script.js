108ab76136a786d3cf1405abe27dee5d193c7b5f
Tang Wai Hoh <waihoh@gmail.com>
2022-02-18 22:21:02 +0800

    add quotes

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('testing')
+// let apiQuotes = [];
+
+// Show new quote
+function newQuote() {
+    // pick a random quote from apiQuotes array
+    // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
+    const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
+    console.log(quote);
+}
+
+// // Get Quotes From API
+// async function getQuotes() {
+//     const apiURL = 'https://type.fit/api/quotes';
+//     try {
+//         const response = await fetch(apiURL);
+//         apiQuotes = await response.json();
+//         newQuote()
+//     } catch (error) {
+//         // Catch Error Here
+//     }
+// }
+
+// On Load
+// getQuotes();
+newQuote();
