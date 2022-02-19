d71892161ef4eb7c4b064a4a928feb447d34d40a
Tang Wai Hoh <waihoh@gmail.com>
2022-02-19 17:01:02 +0800

    completed project

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Udemy JavaScript Web Projects: 20 Projects to Build Your Portfolio. See https://www.udemy.com/course/javascript-web-projects-to-build-your-portfolio-resume/?src=sac&kw=javascript+web+projects
 
 Section 3 - Quote Generator.
+
+Example of the webpage is at https://waihoh.github.io/Javascript-03-Quote-Container/
--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 // let apiQuotes = [];
 
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
 // Show new quote
 function newQuote() {
+    loading();
     // pick a random quote from apiQuotes array
     // const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)]
     const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)]
@@ -24,7 +38,10 @@ function newQuote() {
     } else {
         quoteText.classList.remove('long-quote');
     }
+
+    // Set quote, hide loader
     quoteText.textContent = quote.text;
+    complete();
 }
 
 // Tweet quote
